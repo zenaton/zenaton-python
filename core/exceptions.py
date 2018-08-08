c50f8331e62047f5d017a754ee4af9715d9b497a
Yann Bury <yann.bury@gmail.com>
2018-08-08 16:09:17 +0200

    Exceptions

---
+++ core/exceptions.py
@@ -0,0 +1,25 @@
+
+
+"""Zenaton base error class"""
+class Error(Exception):
+    pass
+
+
+"""Exception raised when communication with workers failed"""
+class InternalError(Exception):
+    pass
+
+
+"""Exception raise when clien code is invalid"""
+class ExternalError(Exception):
+    pass
+
+
+"""Exception raised when wrong argument type is provided"""
+class InvalidArgumentError(ExternalError):
+    pass
+
+
+"""Exception raised when the workflow is unknown"""
+class UnknownWorkflowError(ExternalError):
+    pass
