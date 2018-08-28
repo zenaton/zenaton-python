4342c872b25fde4b2d4e1894779f7b959facdd57
Yann Bury <yann.bury@gmail.com>
2018-08-28 12:13:32 +0200

    Dummy Serializer & Properties classes

--- core/services/properties.py
+++ core/services/properties.py
@@ -1,9 +1,28 @@
-from datetime import date, datetime, time
-
-
 class Properties:
-    SPECIAL_CASES = [date, datetime, time]
 
-    # TO DO
     def blank_instance(self, class_name):
+        pass
+
+    def from_(self, object_):
+        pass
+
+    def set(self, object_, properties):
+        pass
+
+    def object_from(self, class_name, properties, super_class=None):
+        pass
+
+    def check_class(self, object_, super_class):
+        pass
+
+    def valid_object(self, object_, super_class):
+        pass
+
+    def from_comple_type(self, object_):
+        pass
+
+    def set_complex_type(self, object_, props):
+        pass
+
+    def is_special_case(self, object_):
         pass
--- core/services/serializer.py
+++ core/services/serializer.py
@@ -1,2 +1,73 @@
 class Serializer:
-    pass
+
+    def __init__(self):
+        pass
+
+    def encode(self, data):
+        pass
+
+    def decode(self, json_string):
+        pass
+
+    def __is_basic_type(self, data):
+        pass
+
+    def __encode_value(self, value):
+        pass
+
+    def __encode_to_store(self, object_):
+        pass
+
+    def __store_and_encode(self, object_):
+        pass
+
+    def __store_id(self, id):
+        pass
+
+    def __encode_object_by_type(self, object_):
+        pass
+
+    def __encode_object(self, object_):
+        pass
+
+    def __encode_list(self, list_):
+        pass
+
+    def __encode_dict(self, dict_):
+        pass
+
+    def __encode_legacy_dict(self, dict_):
+        pass
+
+    def __is_store_if(self, string_):
+        pass
+
+    def __decode_element(self, value):
+        pass
+
+    def __decode_enumerable(self, enumerable):
+        pass
+
+    def __decode_legacy_list(self, list_):
+        pass
+
+    def __decode_legacy_dict(self, dict_):
+        pass
+
+    def __decode_list(self, list_):
+        pass
+
+    def __decode_dict(self, dict_):
+        pass
+
+    def __decode_from_store(self, id, encoded):
+        pass
+
+    def __decoded_object_by_type(self, id, encoded):
+        pass
+
+    def __decoded_object(self, id, encoded_object):
+        pass
+
+    def __transsform_values(self, dict_):
+        pass
