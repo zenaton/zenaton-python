3a5e01e4772ac1df5b82b2b1a7ffb9d8d54615f4
Yann Bury <yann.bury@gmail.com>
2018-08-30 18:33:52 +0200

    Properties (Ongoing)

--- core/services/properties.py
+++ core/services/properties.py
@@ -1,19 +1,32 @@
 class Properties:
 
-    def blank_instance(self, class_name):
-        pass
+    def blank_instance(self, class_):
+
+        class Empty:
+            pass
+
+        output = Empty()
+        output.__class__ = class_
+        return output
 
     def from_(self, object_):
         pass
 
-    def set(self, object_, properties):
-        pass
+    def set(self, object_, *properties):
+        if properties != (None,) and properties is not None:
+            for (key, value) in properties:
+                setattr(object_, key, value)
 
-    def object_from(self, class_name, properties, super_class=None):
-        pass
+    def object_from(self, class_, properties, super_class=None):
+        object_ = self.blank_instance(class_)
+        self.check_class(object_, super_class)
+        self.set(object_, properties)
+        return object_
 
     def check_class(self, object_, super_class):
-        pass
+        error_message = 'Error - #{object.class} should be an instance of #{super_class}'
+        if not issubclass(type(object_), super_class):
+            raise Exception(error_message)
 
     def valid_object(self, object_, super_class):
         pass
