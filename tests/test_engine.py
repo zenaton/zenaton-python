99a87f5916ee2e0e1242ceb21486150d2975c28d
Yann Bury <yann.bury@gmail.com>
2018-11-07 23:53:26 +0100

    Single Task Dispatch test

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest_mock
 
 from zenaton.exceptions import InvalidArgumentError
 
@@ -18,3 +19,13 @@ def test_valid_job(engine, sequential_workflow, task0):
     assert not engine.valid_job(FakeTask)
     assert engine.valid_job(sequential_workflow)
     assert engine.valid_job(task0)
+
+
+@pytest.mark.usefixtures("client", "engine", "task0")
+def test_dispatch_task(client, engine, task0, mocker):
+    mocker.spy(client, "start_task")
+    task0.dispatch()
+    assert client.start_task.call_count == 1
+
+
+
